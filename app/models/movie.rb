6d1b4e4d79761c181eac1dc76bdab1d9bff139fb
Mark Sun <msun908@berkeley.edu>
2017-09-17 22:46:22 +0000

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
     
     def Movie.get_all_ratings
 		all_ratings = []
-		self.select(:rating).group(:rating).each do |mo|
+		self.select(:rating).unique.each do |mo|
 			all_ratings << mo.rating
 		end
 		return all_ratings
