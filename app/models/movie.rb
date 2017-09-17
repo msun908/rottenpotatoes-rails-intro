4e17316b6d2bc8c69d07850279cf7c67fd1ffa80
Mark Sun <msun908@berkeley.edu>
2017-09-17 22:01:49 +0000

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+    attr_accessible :rating
+    def self.all_ratings
+      	result = {}
+      	self.select(:rating).uniq.each do |movie|
+      		result[movie.rating] = 1
+  	    end
+  	    return result
+    end
 end
